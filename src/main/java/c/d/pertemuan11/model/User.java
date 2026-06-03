39506237e751529ba517af0f7d656a0cc3df62a4
neiranie <email_anda@contoh.com>
2026-06-03 19:08:17 +0700

    Adding User.java

---
+++ src/main/java/c/d/pertemuan11/model/User.java
@@ -0,0 +1,25 @@
+package c.d.pertemuan11.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
