1b8d1427b0cc65897d03a7d0846898e79b8639c5
neiranie <email_anda@contoh.com>
2026-06-03 19:10:32 +0700

    Adding UserDetailsServiceImpl.java

---
+++ src/main/java/c/d/pertemuan11/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package c.d.pertemuan11.service;
+
+import c.d.pertemuan11.model.User;
+import c.d.pertemuan11.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User tidak ditemukan"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
