232ff2ba2fbc316dee2fd761b55e56e45f97bfdc
neiranie <email_anda@contoh.com>
2026-06-03 19:09:24 +0700

    Adding UserRepository.java

---
+++ src/main/java/c/d/pertemuan11/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package c.d.pertemuan11.repository;
+
+import c.d.pertemuan11.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
